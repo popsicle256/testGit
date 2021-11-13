7a081b5918dccc6ac52ae4f43f1b0423e890cbc4
popsicle <popsicle@foxmail.com>
2021-11-13 15:26:00 +0800

    testGit second commit

---
+++ src/main/java/com/popsicle/testgit/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.popsicle.testgit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @program: testGit
+ * @description: TODO
+ * @author: Popsicle
+ * @create: 2021-11-13 15:24
+ **/
+@Controller
+public class UserController {
+
+    @GetMapping("/get")
+    public String get(){
+        return "eure";
+    }
+}
