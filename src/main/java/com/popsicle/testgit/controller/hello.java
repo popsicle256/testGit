9fe31315b94d14c5d2412392eb09606af4af8ac0
popsicle <popsicle@foxmail.com>
2021-11-13 16:43:33 +0800

    testGit fifth commit

--- src/main/java/com/popsicle/testgit/controller/hello.java
+++ src/main/java/com/popsicle/testgit/controller/hello.java
@@ -8,7 +8,6 @@ package com.popsicle.testgit.controller;
  **/
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello world!");
         System.out.println(1+1);
     }
 }
