665f856dcd2637e639b21a231f1f7fbed7fc7649
popsicle <popsicle@foxmail.com>
2021-11-13 15:32:31 +0800

    testGit second commit

---
+++ src/main/java/com/popsicle/testgit/controller/GoodsController.java
@@ -0,0 +1,10 @@
+package com.popsicle.testgit.controller;
+
+/**
+ * @program: testGit
+ * @description: TODO
+ * @author: Popsicle
+ * @create: 2021-11-13 15:31
+ **/
+public class GoodsController {
+}
