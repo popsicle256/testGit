2fedc8973c1b62e87e2750810a4cdb1237935ca8
popsicle <popsicle@foxmail.com>
2021-11-13 15:16:34 +0800

    testGit first commit

---
+++ src/main/java/com/popsicle/testgit/controller/hello.java
@@ -0,0 +1,13 @@
+package com.popsicle.testgit.controller;
+
+/**
+ * @program: testGit
+ * @description: TODO
+ * @author: Popsicle
+ * @create: 2021-11-13 15:14
+ **/
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
